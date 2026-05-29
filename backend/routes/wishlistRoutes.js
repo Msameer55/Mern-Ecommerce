4a283002afa5e30e2b13bc07ef5179cdaded633f
Sameer <msameer427081@gmail.com>
2026-05-29 05:24:15 +0500

    wishlist routes work

--- backend/routes/wishlistRoutes.js
+++ backend/routes/wishlistRoutes.js
@@ -1,9 +1,11 @@
 import express from "express"
-import { addProductToWishlist, deleteProductFromWishlist, getWishlistItems } from "../controllers/wishlistController.js";
+import { addProductToWishlist, deleteProductFromWishlist, getWishlistItems, mergeWishlist } from "../controllers/wishlistController.js";
+import { verifyToken } from "../middlewares/protectedRoutes.js";
 const wishlistRoutes = express.Router();
 
 wishlistRoutes.get("/", getWishlistItems);
 wishlistRoutes.post("/", addProductToWishlist);
 wishlistRoutes.delete("/:id", deleteProductFromWishlist);
+wishlistRoutes.post("/merge", verifyToken, mergeWishlist);
 
 export default wishlistRoutes;
